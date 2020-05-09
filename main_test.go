17858cff22e475e7705de9506540fc7a4e9f1149
Tianchi Liu <ltcwendell@gmail.com>
2020-05-10 00:55:49 +0800

    init

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func Test_add(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test",
+			args{
+				10,
+				20,
+			},
+			30,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
